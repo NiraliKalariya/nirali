e8c656c135489c04d68d5c05d40f539f0fee5b1c
NiraliKalariya <ndkalariya@gmail.com>
2018-08-28 22:15:34 +0530

    Update interstion.java

--- src/main/java/Insertion.java
+++ src/main/java/Insertion.java
@@ -8,7 +8,7 @@ public class Insertion {
         a[i] = a[j];
         a[j] = temp;
     }
-
+    // Sort function
     private static void sort(Comparable[] a) {
         for (int i = 0; i < a.length; i++) {
             for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
